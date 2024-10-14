a02a3d9cc2fecac3d6892bb5a831ab7f6a6aa8c2
Shuvkant <weird426344@gmail.com>
2024-10-14 08:27:05 +0545

    selecting, creating and deleting

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -51,8 +51,21 @@ console.log(getElementsByClassName);
 //Creating and inserting elements
 //.insertAdjacentHtml
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML = 'We use cookied for improved functionality and 
-analytics. <button class="btn btn--close-cookie">Got it! </button>';
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML = `We use cookies for improved functionality and 
+analytics. <button class="btn btn--close-cookie">Got it!</button>`;
 
+const header = document.querySelector("header"); // or a specific class or ID
+
+header.prepend(message);
+header.append(message);
+
+//delete Elements
+document.querySelector(".btn--close-cookie").addEventListener(
+  "click",
+  function () {
+    //   message.remove();
+    message.parentElement.removeChild(message);
+  },
+);
