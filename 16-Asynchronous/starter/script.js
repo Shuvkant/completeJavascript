535a5b539a87957a5cb65179a0dcfe216b93f302
Shuvkant <weird426344@gmail.com>
2024-10-18 19:31:02 +0545

    Asychronous iusrgnvadf

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -37,9 +37,4 @@ console.log("Test Start");
 setTimeout(() => console.log("0 sec timer"), 0);
 Promise.resolve("Resolved promise 1").then((res) => console.log(res));
 
-Promise.resolve("Resolved promise 2").then((res) => {
-  for (let i = 0; i < 1000000; i++) {
-    console.log(res);
-  }
-});
 console.log("Test end");
